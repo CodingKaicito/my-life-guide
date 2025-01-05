d4de73325fe18855bb34bf77788017446ff875a5
CodingKaicito <kaidiaz60@gmail.com>
2025-01-05 01:40:53 -0600

    jan 5th 2025 @ 1:40

--- man.js
+++ man.js
@@ -1,5 +1,17 @@
-sap.ui.define(["sap/m/Text"], function(Text) {
-    new Text({
-        text: "OpenUI5 is loaded successfully!"
-    }).placeAt("content");
-});
+(function () {
+    const nav = document.querySelector('nav');
+    let isScrolling;
+
+    window.addEventListener("scroll", function () {
+        clearTimeout(isScrolling);
+
+        isScrolling = setTimeout(() => {
+            const scrollPosition = document.documentElement.scrollTop || window.pageYOffset;
+            if (scrollPosition > 20) {
+                nav.classList.add("sticky");
+            } else {
+                nav.classList.remove("sticky");
+            }
+        }, 50); // Adjust the delay as needed
+    });
+})();
