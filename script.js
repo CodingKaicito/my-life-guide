510fba67a4dd8b2db6f0cd4a6c212f2f740bc6ca
CodingKaicito <kaidiaz60@gmail.com>
2025-01-19 17:49:02 -0600

    Jan 19, 2025 at 17:49

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ type="text/javascript"
     function checkPswd() {
         var confirmPassword = "tips";
         var password = document.getElementById("pswd").value;
+        console.log("Password entered:", password);
         if (password == confirmPassword) {
             window.location="life guide/index.html";
         }
