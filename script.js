5e146c117de7eb05c3dbb078990892c321de68d6
CodingKaicito <kaidiaz60@gmail.com>
2025-01-19 18:56:57 -0600

    Jan 19, 2025 at 18:56

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 type="text/javascript"
     function checkPswd() {
-        var confirmPassword = "im just an awkward guy";
+        var confirmPassword = "im an awkward guy";
         var password = document.getElementById("pswd").value;
         console.log("Password entered:", password);
         if (password == confirmPassword) {
